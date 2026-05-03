1569e67a9cf138c6a3e9018ecdf83827db31f8a2
eloantg-alt <eloan.tg@gmail.com>
2026-05-03 15:00:37 +0200

    Start

---
+++ i18n.js
@@ -0,0 +1,14 @@
+const translations = {
+    en: {
+        //Nav
+        'nav.home': 'Home',
+        'nav.about': 'About',
+        'nav.watchlist': 'Watchlist',
+    },
+    fr: {
+        //Nav
+        'nav.home': 'Accueil',
+        'nav.about': 'À propos',
+        'nav.watchlist': 'Watchlist',
+    },
+};
